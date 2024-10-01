00ddb2885ed00e45331499a117ccbda38521f7d7
saeidboghraei202@gmail.com <SaeidBoghraei>
2024-10-01 11:30:33 +0330

    end design  home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:signal/signal_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'vazir'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.indigo[160],
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+              ),
+              SizedBox(height: 80),
+              Image.asset(
+                'images/p.png',
+                width: 380,
+              ),
+              SizedBox(height: 80),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal, minimumSize: Size(300, 50)),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SignalScreen()),
+                  );
+                },
+                child: Text(
+                  'ورود به حساب',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, minimumSize: Size(300, 50)),
+                onPressed: () {},
+                child: Text(
+                  'ثبت نام ',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: () {},
+                child: Text('فراموشی رمز'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/signal_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignalScreen extends StatelessWidget {
+  const SignalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
