d6ecbcbfee111f4d412f7a02a9837a80d67805d5
saeidboghraei202@gmail.com <SaeidBoghraei>
2024-10-01 14:33:44 +0330

    end design signal

--- lib/signal_screen.dart
+++ lib/signal_screen.dart
@@ -6,11 +6,100 @@ class SignalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text(
+          'VIP سیگنال های ',
+          style: TextStyle(
+            fontFamily: 'vazir',
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('hi'),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+              ),
+              getItem('r'),
+              getItem('c'),
+              getItem('a'),
+              getItem('s'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  getItem(String imageName) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          width: 400,
+          height: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/$imageName.png'),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'فروش روی : 160000',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.red,
+                            fontFamily: 'vazir',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.trending_down),
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 17,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'خرید روی : 149000',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.green,
+                            fontFamily: 'vazir',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.trending_up),
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(
+                color: Colors.black,
+                height: 5,
+                thickness: 0.8,
+              ),
+            ],
+          ),
         ),
       ),
     );
